bcfbab18dd910104a73445ee134082f36478e039
KyleEvan <kep6634@rit.edu>
2016-03-30 01:51:03 -0400

    'inspire'

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,9 +1,16 @@
 var express = require('express');
 var router = express.Router();
+var artistData = require('./artist_wrapper.json');
+var i = 0; // artist index
+//console.log(artistData);
+console.log(artistData.artist[i].influences);
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { name: artistData.artist[i].artistName, decade: artistData.artist[i].yearsActive, influences:artistData.artist[i].influences });
 });
 
 module.exports = router;
+/*
+C:\Users\Owner\Desktop\inspire
+*/
