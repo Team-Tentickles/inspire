1cbe6f6e7c327ec71516e8a4761a2a0e01566d6e
Zach Davies <ztd4349@rit.edu>
2016-03-30 22:53:22 -0400

    Added functions to use APIs, ready to be connected to front end

--- app/io.js
+++ app/io.js
@@ -1,8 +1,14 @@
+var utils = require('./public/js/utils.js')
 var socketio = require('socket.io');
 var io = socketio();
 
 io.sockets.on("connection", function(socket) {
 	console.log("Aye");
+	utils.makePackage({'first': 'Taylor Swift', 'second': 'Kelly Clarkson'}, socket);
+
+	socket.on('serverArtist', function(data){
+		utils.makePackage(data, socket);
+	});
 });
 
 module.exports = io;
---
+++ app/public/js/config/config.js
@@ -0,0 +1,19 @@
+var config = {};
+
+config.rovi = {
+	key:'q54m33fz7x9mhxrh5247bcpj',
+	secret:'XgQYZjXQ4T'
+};
+
+config.echo = {
+	key: '0TXV799W5Q4FCHB8B',
+	consumer:'0a404e1c48fc20850378baa472013baa',
+	shared: 'adnm2CCHRPezfl74f3nqpA'
+};
+
+config.flickr = {
+	key: '181daac5cf4e9c9d278496da080b7b9e',
+	secret: '00668447d00bdae7'
+}
+
+module.exports = config;
